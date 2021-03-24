e058215c89fd658b855d72f787edb67e03e94416
billy34 <cornu.g@cirad.fr>
2021-03-24 20:04:29 +0100

    * use utility functions for // and progress

--- R/scglrCrossVal.r
+++ R/scglrCrossVal.r
@@ -28,7 +28,6 @@ if(getRversion()>="2.15.1") {
 #' @param mc.cores deprecated
 #' @param method Regularization criterion type. Object of class "method.SCGLR"
 #' built by \code{\link{methodSR}} for Structural Relevance.
-#' @param progress if TRUE display a progress bar (default to TRUE if in interactive mode, needs \code{progressr} package)
 #' @param nfolds deprecated. Use \code{fold} parameter instead.
 #' @return  a matrix containing the criterion values for each response (rows) and each number of components (columns).
 #' @references Bry X., Trottier C., Verron T. and Mortier F. (2013) Supervised Component Generalized Linear Regression using a PLS-extension of the Fisher scoring algorithm. \emph{Journal of Multivariate Analysis}, 119, 47-60.
@@ -64,7 +63,7 @@ if(getRversion()>="2.15.1") {
 #' #plot(mean.crit, type="l")
 #' }
 scglrCrossVal <-  function(formula,data,family,K=1,folds=10,type="mspe",size=NULL,offset=NULL,
-                           na.action=na.omit,crit=list(), method=methodSR(), progress=interactive(), nfolds, mc.cores) {
+                           na.action=na.omit,crit=list(), method=methodSR(), nfolds, mc.cores) {
   
   if(!missing(nfolds)) {
     .Deprecated("fold", msg="'nfolds' parameter as been renamed to 'folds'. I'll use provided value but update your code!")
@@ -218,7 +217,7 @@ scglrCrossVal <-  function(formula,data,family,K=1,folds=10,type="mspe",size=NUL
     if(inherits(try_result,"try-error")) {
       # return error as a warning
       warning(attr(try_result,"condition")$message,", in fold ",nf, call. = FALSE)
-      prog()
+      ..progressor..()
       return(list(cv=array(NA,c(ny,K)),cvNull=cvNull))
     }
 
@@ -257,21 +256,13 @@ scglrCrossVal <-  function(formula,data,family,K=1,folds=10,type="mspe",size=NUL
       }
 
     }
-    prog()
+    ..progressor..()
     return(list(cv=cv,cvNull=cvNull))
   }
 
-  if(isNamespaceLoaded("future.apply")) fiter <- future.apply::future_lapply else fiter <- lapply
-  if(progress&&require("progressr")) {
-    progressr::with_progress({
-      prog <- progressr::progressor(kfolds)
-      result <- fiter(seq(kfolds), mainFolds)
-    })
-  } else {
-    prog <- function() {}
-    result <- fiter(seq(kfolds), mainFolds)
-  }
-
+  ..progressor.. <- getProgressor(kfolds)
+  result <- getParallel("lapply", seq(kfolds), mainFolds)
+  
   cv <- sapply(result,function(x) x$cv,simplify=FALSE)
   cv <- simplify2array(cv)
   cv <- apply(cv,c(1,2),mean, na.rm=TRUE)
